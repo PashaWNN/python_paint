20b8091a79c83c61ed2ecdc8427b16abbc1ab0ee
PashaWNN <pashawnn@gmail.com>
2020-01-09 22:03:28 +0900

    Implement Bresenham lines Move algorithms to other file

---
+++ algorhitms.py
@@ -0,0 +1,96 @@
+def ellipse(rx, ry, xc, yc, callback):
+    """Draw and ellipse"""
+    x = 0
+    y = ry
+
+    # Initial decision parameter of region 1  
+    d1 = ((ry * ry) - (rx * rx * ry) +
+          (0.25 * rx * rx))
+    dx = 2 * ry * ry * x
+    dy = 2 * rx * rx * y
+
+    # For region 1  
+    while (dx < dy):
+
+        # Print points based on 4-way symmetry  
+        callback(x + xc, y + yc)
+        callback(-x + xc, y + yc)
+        callback(x + xc, -y + yc)
+        callback(-x + xc, -y + yc)
+
+        # Checking and updating value of  
+        # decision parameter based on algorithm  
+        if (d1 < 0):
+            x += 1
+            dx = dx + (2 * ry * ry)
+            d1 = d1 + dx + (ry * ry)
+        else:
+            x += 1
+            y -= 1
+            dx = dx + (2 * ry * ry)
+            dy = dy - (2 * rx * rx)
+            d1 = d1 + dx - dy + (ry * ry)
+
+            # Decision parameter of region 2  
+    d2 = (((ry * ry) * ((x + 0.5) * (x + 0.5))) +
+          ((rx * rx) * ((y - 1) * (y - 1))) -
+          (rx * rx * ry * ry))
+
+    # Plotting points of region 2  
+    while (y >= 0):
+
+        # printing points based on 4-way symmetry  
+        callback(x + xc, y + yc)
+        callback(-x + xc, y + yc)
+        callback(x + xc, -y + yc)
+        callback(-x + xc, -y + yc)
+
+        # Checking and updating parameter  
+        # value based on algorithm  
+        if (d2 > 0):
+            y -= 1
+            dy = dy - (2 * rx * rx)
+            d2 = d2 + (rx * rx) - dy
+        else:
+            y -= 1
+            x += 1
+            dx = dx + (2 * ry * ry)
+            dy = dy - (2 * rx * rx)
+            d2 = d2 + dx - dy + (rx * rx)
+
+
+def naive_line(x1, y1, x2, y2, callback):
+    """Draw a line with naive algorithm"""
+    dx = x2 - x1
+    dy = y2 - y1
+
+    for x in range(min(x1, x2), max(x1, x2)):
+        y = int(y1 + dy * (x - x1) / dx)
+        callback(x, y)
+
+
+def bresenham_line(x1, y1, x2, y2, callback):
+    dx = x2 - x1
+    dy = y2 - y1
+
+    xsign = 1 if dx > 0 else -1
+    ysign = 1 if dy > 0 else -1
+
+    dx = abs(dx)
+    dy = abs(dy)
+
+    if dx > dy:
+        xx, xy, yx, yy = xsign, 0, 0, ysign
+    else:
+        dx, dy = dy, dx
+        xx, xy, yx, yy = 0, ysign, xsign, 0
+
+    D = 2 * dy - dx
+    y = 0
+
+    for x in range(dx + 1):
+        callback(x1 + x * xx + y * yx, y1 + x * xy + y * yy)
+        if D >= 0:
+            y += 1
+            D -= 2 * dx
+        D += 2 * dy
--- tools.py
+++ tools.py
@@ -1,10 +1,10 @@
-from tkinter import PhotoImage
+from algorhitms import ellipse, bresenham_line
 
 
-def _safe_put(img, x, y):
+def _safe_put(img, x, y, color='#000000'):
     if x < 0 or y < 0:
         return
-    img.put('#000000', (x, y))
+    img.put(color, (x, y))
 
 
 class Tool:
@@ -24,16 +24,18 @@ class Tool:
 class Pencil(Tool):
     @classmethod
     def handle_press(cls, x, y, img):
-        pass
+        img.ctx['last_x'] = x
+        img.ctx['last_y'] = y
 
     @classmethod
     def handle_motion(cls, x, y, img):
-        print(x, y)
-        _safe_put(img, x, y)
+        def callback(x, y):
+            _safe_put(img, x, y)
 
-    @classmethod
-    def handle_release(cls, x, y, img):
-        pass
+        bresenham_line(img.ctx['last_x'], img.ctx['last_y'], x, y, callback)
+
+        img.ctx['last_x'] = x
+        img.ctx['last_y'] = y
 
 
 class Line(Tool):
@@ -48,16 +50,10 @@ class Line(Tool):
         x2, y2 = x, y
         x1, y1 = img.ctx['start_x'], img.ctx['start_y']
 
-        dx = x2 - x1
-        dy = y2 - y1
-
-        for x in range(min(x1, x2), max(x1, x2)):
-            y = int(y1 + dy * (x - x1) / dx)
+        def callback(x, y):
             _safe_put(img, x, y)
 
-    @classmethod
-    def handle_release(cls, x, y, img):
-        pass
+        bresenham_line(x1, y1, x2, y2, callback)
 
 
 class Oval(Tool):
@@ -73,67 +69,10 @@ class Oval(Tool):
         rx = abs(xc - x)
         ry = abs(yc - y)
 
-        x = 0
-        y = ry
+        def callback(x, y):
+            _safe_put(img, x, y)
 
-        # Initial decision parameter of region 1  
-        d1 = ((ry * ry) - (rx * rx * ry) +
-              (0.25 * rx * rx))
-        dx = 2 * ry * ry * x
-        dy = 2 * rx * rx * y
-
-        # For region 1  
-        while (dx < dy):
-
-            # Print points based on 4-way symmetry  
-            _safe_put(img, x + xc, y + yc)
-            _safe_put(img, -x + xc, y + yc)
-            _safe_put(img, x + xc, -y + yc)
-            _safe_put(img, -x + xc, -y + yc)
-
-            # Checking and updating value of  
-            # decision parameter based on algorithm  
-            if (d1 < 0):
-                x += 1
-                dx = dx + (2 * ry * ry)
-                d1 = d1 + dx + (ry * ry)
-            else:
-                x += 1
-                y -= 1
-                dx = dx + (2 * ry * ry)
-                dy = dy - (2 * rx * rx)
-                d1 = d1 + dx - dy + (ry * ry)
-
-                # Decision parameter of region 2  
-        d2 = (((ry * ry) * ((x + 0.5) * (x + 0.5))) +
-              ((rx * rx) * ((y - 1) * (y - 1))) -
-              (rx * rx * ry * ry))
-
-        # Plotting points of region 2  
-        while (y >= 0):
-
-            # printing points based on 4-way symmetry  
-            _safe_put(img, x + xc, y + yc)
-            _safe_put(img, -x + xc, y + yc)
-            _safe_put(img, x + xc, -y + yc)
-            _safe_put(img, -x + xc, -y + yc)
-
-            # Checking and updating parameter  
-            # value based on algorithm  
-            if (d2 > 0):
-                y -= 1
-                dy = dy - (2 * rx * rx)
-                d2 = d2 + (rx * rx) - dy
-            else:
-                y -= 1
-                x += 1
-                dx = dx + (2 * ry * ry)
-                dy = dy - (2 * rx * rx)
-                d2 = d2 + dx - dy + (rx * rx)
-
-    @classmethod
-    def handle_release(cls, x, y, img):
-        pass
+        ellipse(rx, ry, xc, yc, callback)
 
 
 tools_list = [Pencil, Line, Oval]
